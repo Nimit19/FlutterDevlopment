0df2324ad9161c7582aa5b6691986022078518ad
nimit <nimitpatelnp19@gmail.com>
2023-03-07 12:39:48 +0530

    Initial Commit

---
+++ Udemy Course/Flutter Apps/my_shop/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:my_shop/screens/product_detail_screen.dart';
+import 'package:my_shop/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/products.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
+    );
+  }
+}
